13fd8a6d08c0f42137fd9da0a0f7336214a8c17e
AronBakaBalint <44266507+AronBakaBalint@users.noreply.github.com>
2020-10-16 17:28:32 +0300

    removed advance payment

--- backend/src/main/java/aron/utcn/licenta/service/impl/ParkingSpotManagementServiceImpl.java
+++ backend/src/main/java/aron/utcn/licenta/service/impl/ParkingSpotManagementServiceImpl.java
@@ -1,21 +1,16 @@
 package aron.utcn.licenta.service.impl;
 
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import aron.utcn.licenta.model.ParkingSpot;
-import aron.utcn.licenta.model.Person;
 import aron.utcn.licenta.model.Reservation;
 import aron.utcn.licenta.repository.ParkingPlaceRepository;
-import aron.utcn.licenta.repository.PersonRepository;
 import aron.utcn.licenta.repository.ReservationRepository;
 import aron.utcn.licenta.service.ArduinoService;
 import aron.utcn.licenta.service.ParkingSpotManagementService;
@@ -25,15 +20,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ParkingSpotManagementServiceImpl implements ParkingSpotManagementService {
 
-	private final Environment environment;
-
 	private final ArduinoService arduinoService;
 
 	private final ParkingPlaceRepository parkingPlaceRepository;
 
 	private final ReservationRepository reservationRepository;
-
-	private final PersonRepository personRepository;
 
 	@Override
 	public List<ParkingSpot> getAllParkingPlaces() {
@@ -79,25 +70,11 @@ public class ParkingSpotManagementServiceImpl implements ParkingSpotManagementSe
 			} else {
 				ParkingSpot parkingPlace = parkingPlaceRepository.findById(reservation.getParkingSpotId());
 				if (parkingPlace.isOccupied()) {
-					Person person = personRepository.findById(parkingPlace.getUser().getId());
-					double price = calculatePrice(parkingPlace);
-					DecimalFormat df = new DecimalFormat("#.##");
-					df.setRoundingMode(RoundingMode.CEILING);
-					price = Double.valueOf(df.format(price));
-					if (person.hasEnoughMoney(price)) {
-						person.pay(price);
-						reservation.setFinished();
-						parkingPlace.setFree();
-						df.setRoundingMode(RoundingMode.FLOOR);
-						displayOnLCD(price + " lei;" + df.format(person.getBalance()));
-					} else {
-						displayOnLCD("no money");
-					}
+					reservation.setFinished();
+					parkingPlace.setFree();
+					displayOnLCD("Goodbye");
 				} else {
 					openBarrier();
-					Person user = reservation.getUser();
-					String reservationCost = environment.getProperty("parking.reservation_cost");
-					user.addMoney(Double.parseDouble(reservationCost));
 					reservation.setOccupied();
 					parkingPlace.setOccupied();
 					parkingPlace.setArrivalTime(new Date());
@@ -107,26 +84,18 @@ public class ParkingSpotManagementServiceImpl implements ParkingSpotManagementSe
 			displayOnLCD("no reservation found");
 		}
 	}
-	
+
 	@Override
 	@Transactional
 	public void clearUnoccupiedPlaces() {
 		List<ParkingSpot> parkingPlaces = parkingPlaceRepository.getAllParkingPlaces();
 		Date currentDate = new Date();
-		parkingPlaces.forEach(pp->{
-			if(pp.isReserved() && getTimeDiff(pp.getReservation().getReservationDate(), currentDate) > 30/**60*/ ) {
+		parkingPlaces.forEach(pp -> {
+			if (pp.isReserved() && getTimeDiff(pp.getReservation().getReservationDate(), currentDate) > 30 * 60) {
 				pp.getReservation().cancel();
 				pp.setFree();
 			}
 		});
-	}
-
-	private float calculatePrice(ParkingSpot parkingPlace) {
-		Date departureTime = new Date();
-		Date arrivalTime = parkingPlace.getArrivalTime();
-		float diff = getTimeDiff(departureTime, arrivalTime);
-		float pricePerHour = Float.parseFloat(environment.getProperty("parking.price_per_hour"));
-		return diff * pricePerHour / 3600;
 	}
 
 	private void displayOnLCD(String message) {
@@ -136,12 +105,11 @@ public class ParkingSpotManagementServiceImpl implements ParkingSpotManagementSe
 	private void openBarrier() {
 		arduinoService.activateBarrier();
 	}
-	
+
 	private float getTimeDiff(Date start, Date finish) {
 		long diffInMillies = Math.abs(start.getTime() - finish.getTime());
 		long diff = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.MILLISECONDS);
 		return diff;
 	}
-	
 
 }
