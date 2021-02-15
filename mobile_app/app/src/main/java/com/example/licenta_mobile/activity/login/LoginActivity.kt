d046f05834f300ad2c78cc57b05ed9fab583f9d9
Aron Baka <aronbaka97@yahoo.com>
2021-02-15 10:00:15 +0200

    refactored login logic

--- mobile_app/app/src/main/java/com/example/licenta_mobile/activity/login/LoginActivity.kt
+++ mobile_app/app/src/main/java/com/example/licenta_mobile/activity/login/LoginActivity.kt
@@ -1,65 +1,25 @@
 package com.example.licenta_mobile.activity.login
 
 import android.content.Intent
-import android.os.Bundle
-import android.os.StrictMode
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.licenta_mobile.activity.main.ParkingActivity
+import android.os.Bundle
 import com.example.licenta_mobile.R
 import com.example.licenta_mobile.activity.main.MainActivity
-import com.example.licenta_mobile.activity.register.RegistrationActivity
-import com.example.licenta_mobile.dto.LoginRequestDto
-import com.example.licenta_mobile.model.UserData.update
-import com.example.licenta_mobile.rest.LoginService
-import com.example.licenta_mobile.rest.RestClient.client
-import com.example.licenta_mobile.security.Token
-import com.google.android.material.textfield.TextInputEditText
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
-class LoginActivity : AppCompatActivity() {
-
-    private val loginService = client!!.create(LoginService::class.java)
+class LoginActivity : AppCompatActivity(), LoginCommandListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_login)
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
+        setContentView(R.layout.login_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, LoginFragment.newInstance())
+                    .commitNow()
+        }
     }
 
-    fun loginRequest(view: View?) {
-        val username = (findViewById<View>(R.id.username) as TextInputEditText).text.toString()
-        val password = (findViewById<View>(R.id.password) as TextInputEditText).text.toString()
-        val loginRequestDto = LoginRequestDto(username, password)
-        val call = loginService.authenticate(loginRequestDto)
-        call.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()!!
-                    if ("false" == responseBody) {
-                        Toast.makeText(this@LoginActivity, "Incorrect Username or Password", Toast.LENGTH_LONG).show()
-                    } else {
-                        Token.jwtToken = responseBody
-                        update()
-                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                        startActivity(intent)
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Toast.makeText(this@LoginActivity, "Connection Error! Please try again later!", Toast.LENGTH_LONG).show()
-                call.cancel()
-            }
-        })
-    }
-
-    fun register(view: View?) {
-        val intent = Intent(this@LoginActivity, RegistrationActivity::class.java)
+    override fun onLogin() {
+        val intent = Intent(LoginActivity@this, MainActivity::class.java)
         startActivity(intent)
     }
 }
